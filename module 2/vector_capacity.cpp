62437143500daa72a78a068821e2963e2e5384fb
TashdidDiganta <tashdiddiganta@gmail.com>
2025-12-16 13:05:09 +0600

    Vector capacity

---
+++ module 2/vector_capacity.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+   vector<int> v; 
+   cout << v.capacity() << endl;
+   v.push_back(10);
+   v.push_back(120);
+   v.push_back(30);
+   cout << v.size() << endl;
+   v.resize(2);
+   cout << v.size() << endl;
+   return 0;
+}
